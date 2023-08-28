ea44d677f31fad5bf33cac90dcd1271385dbffe0
Brahim HM <brahimhm98@gmail.com>
2023-08-28 23:44:56 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader=document.createElement('h1')
+newHeader.id='victory'
+newHeader.innerHTML="Brahim is the champion"
+const main=document.querySelector('main#main')
+main.remove()
